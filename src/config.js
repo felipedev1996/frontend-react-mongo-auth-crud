d0829b90239cb9c840cd9e2c145ef3a50cb2bb68
felipedev1996 <felipepino1996@gmail.com>
2023-08-06 23:53:54 -0400

    render deploy error axios

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "https://backend-react-auth.onrender.com"|| "http://localhost:4000/api";
+export const API_URL = import.meta.env.VITE_API_URL|| "https://backend-react-auth.onrender.com/api"|| "http://localhost:4000/api";
 // export const API_URL = "http://localhost:4000/api";
