5c23fece2f70b49279d9d49c01f43fa8c43a5f6a
felipedev1996 <felipepino1996@gmail.com>
2023-08-14 11:45:52 -0400

    changes in TaskCard component

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <TaskProvider>
         <BrowserRouter>
           <main className="container content-container mx-auto px-10 md:px-0">
-            <Navbar />
+            {/* <Navbar /> */}
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export function Navbar() {
   // console.log(isAuthenticated, user)
 
   return (
-    <nav className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg">
+    <nav className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg ">
       <h1 className="text-2xl font-bold">
         <Link to={isAuthenticated ? "/tasks" : "/"}>Task Manager</Link>
       </h1>
@@ -21,18 +21,20 @@ export function Navbar() {
               <ButtonLink to="/add-task">Add Task</ButtonLink>
             </li>
             <li>
+              <ButtonLink>
               <Link to="/" onClick={() => logout()}>
                 Logout
               </Link>
+              </ButtonLink>
             </li>
           </>
         ) : (
           <>
             <li>
-              <ButtonLink to="/login">Login</ButtonLink>
+              <ButtonLink to="/login" classname={"inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"}>Login</ButtonLink>
             </li>
             <li>
-              <ButtonLink to="/register">Register</ButtonLink>
+              <ButtonLink to="/register" classname={"inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"} >Register</ButtonLink>
             </li>
           </>
         )}
