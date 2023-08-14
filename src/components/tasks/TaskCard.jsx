873be6701f6a67ed223e79d870a0efd839c38764
felipedev1996 <felipepino1996@gmail.com>
2023-08-14 00:16:19 -0400

    change to card task

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,8 +1,9 @@
 import { useTasks } from "../../context/tasksContext";
-// import { Button, ButtonLink, Card } from "../ui";
+
 
     import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { ButtonLink } from "../ui/ButtonLink";
 
 export function TaskCard({ task }) {
   const { deleteTask } = useTasks();
@@ -12,41 +13,44 @@ export function TaskCard({ task }) {
 
   return (
     <Card>
-      <Card.Header as="h5">Featured</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
+      <header className="flex justify-between">
+        <h1 className="text-2xl font-bold">{task.title}</h1>
         <div className="flex gap-x-2 items-center">
           <Button onClick={() => deleteTask(task._id)}>Delete</Button>
-         <ButtonLink to={`/tasks/${task._id}`}>Edit</ButtonLink>
+          <ButtonLink to={`/tasks/${task._id}`}>Edit</ButtonLink>
         </div>
-      </Card.Body>
+      </header>
+      <p className="text-slate-300">{task.description}</p>
+      {/* format date */}
+      <p>
+        {task.date &&
+          new Date(task.date).toLocaleDateString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+      </p>
     </Card>
-  );
+   );
   }
+    // <Card>
+    //   <Card.Header as="h5">Featured</Card.Header>
+    //   <Card.Body>
+    //     <Card.Title>{task.title}</Card.Title>
+    //     <Card.Text>
+    //      {task.description}
+    //     </Card.Text>
+    //     <div className="flex gap-x-2 items-center">
+    //       <Button variant=" outline-success"  onClick={() => deleteTask(task._id)}>Delete</Button>
+    //      <ButtonLink to={`/tasks/${task._id}`}>Edit</ButtonLink>
+    //     </div>
+    //   </Card.Body>
+    //   <Card.Link>
+    //     <Button variant="primary">Go somewhere</Button>
+    //   </Card.Link>
+    // </Card>
+ 
 
 export default TaskCard;
-    // <Card>
-    //   <header className="flex justify-between">
-    //     <h1 className="text-2xl font-bold">{task.title}</h1>
-    //     <div className="flex gap-x-2 items-center">
-    //       <Button onClick={() => deleteTask(task._id)}>Delete</Button>
-    //       <ButtonLink to={`/tasks/${task._id}`}>Edit</ButtonLink>
-    //     </div>
-    //   </header>
-    //   <p className="text-slate-300">{task.description}</p>
-    //   {/* format date */}
-    //   <p>
-    //     {task.date &&
-    //       new Date(task.date).toLocaleDateString("en-US", {
-    //         weekday: "long",
-    //         year: "numeric",
-    //         month: "long",
-    //         day: "numeric",
-    //       })}
-    //   </p>
-    // </Card>
-  // );
-// }
+   
