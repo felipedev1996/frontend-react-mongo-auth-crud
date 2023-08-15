87d7441819446105b6644f367795e5e109ec5e6f
felipedev1996 <felipepino1996@gmail.com>
2023-08-15 18:55:07 -0400

    change path backend

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL =  "https://mern-auth-mongo.azurewebsites.net/api";
+export const API_URL =  " https://backend-react-auth.onrender.com/api";
 // export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
