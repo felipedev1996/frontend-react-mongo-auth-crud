8122e3781729278ab009386703bf2748b2d4e43f
felipedev1996 <felipepino1996@gmail.com>
2023-08-16 00:14:16 -0400

    fix ui buttons

--- src/components/ui/ButtonLink.jsx
+++ src/components/ui/ButtonLink.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 
 export const ButtonLink = ({ to, children ,classname}) => (
-  <Link to={to} >
+  <Link to={to} {...classname} >
     {children}
+   
   </Link>
 );
