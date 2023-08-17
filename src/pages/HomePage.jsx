b002c3116e61e34a726846be535ae1c2f25545c9
felipedev1996 <felipepino1996@gmail.com>
2023-08-16 20:18:08 -0400

    Update Home Page new style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,14 +4,13 @@ function HomePage() {
   return (
   <section className="bg-red-500 flex justify-center items-center">
     <header className="bg-zinc-800 p-10">
-      <h1 className="text-5xl py-2 font-bold">React Tasks</h1>
+      <h1 className="text-5xl py-2 font-bold">React version Beta</h1>
       <p className="text-md text-slate-400">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
-        fugit doloremque molestias recusandae labore repellat amet dicta tempore
-        necessitatibus facilis repellendus voluptas ducimus maiores deserunt sed
-        quo ratione provident debitis aut, voluptatem aliquam iste blanditiis
-        ex? Voluptatibus, fuga quasi necessitatibus cumque optio error enim,
-        officia accusantium vitae doloremque, molestias modi.
+       Aplicacion para gestionar tareas,creada con React,Node y MongoDB,
+       utilizando principalmente , react-router-dom, react-hook-form, Tailwind-css .
+       Conectada a la api backend que esta en le repositoria a continuacion <Link to={"https://github.com/felipedev1996/backend-react-mongo-auth-crud"} className="text-blue-500 hover:text-blue-700 font-bold py-2 px-4 rounded">backend-react-mongo-auth-crud</Link>
+
+       
       </p>
 
       <Link
