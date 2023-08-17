8df3323875e30c9d990c5098c104a61778148492
felipedev1996 <felipepino1996@gmail.com>
2023-08-16 20:24:09 -0400

    Update Home Page new styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,9 +6,11 @@ function HomePage() {
     <header className="bg-zinc-800 p-10">
       <h1 className="text-5xl py-2 font-bold">React version Beta</h1>
       <p className="text-md text-slate-400">
-       Aplicacion para gestionar tareas,creada con React,Node y MongoDB,
-       utilizando principalmente , react-router-dom, react-hook-form, Tailwind-css .
-       Conectada a la api backend que esta en le repositoria a continuacion <Link to={"https://github.com/felipedev1996/backend-react-mongo-auth-crud"} className="text-blue-500 hover:text-blue-700 font-bold py-2 px-4 rounded">backend-react-mongo-auth-crud</Link>
+       
+         Aplicacion para gestionar tareas,creada con React,Node y MongoDB,
+         utilizando librerias como, react-router-dom, react-hook-form, Tailwind-css .
+         Conectada a la api backend que esta en el repositorio a continuacion
+       <Link to={"https://github.com/felipedev1996/backend-react-mongo-auth-crud"} className="text-blue-500 hover:text-blue-700 font-bold py-2 px-4 rounded">backend-react-mongo-auth-crud</Link>
 
        
       </p>
